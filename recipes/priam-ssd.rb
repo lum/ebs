4320fa95a9b5c39bcb47e0e1719c908679ca435d
Steve <steve.lum@gmail.com>
2014-12-15 12:51:13 -0800

    adding in new priam-ssd recipe

---
+++ recipes/priam-ssd.rb
@@ -0,0 +1,54 @@
+#
+# Cookbook Name:: ebs
+# Recipe:: priam-ssd
+#
+# Copyright 2014, Steve Lum
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+
+case node[:platform]
+when 'debian','ubuntu'
+  package 'xfsdump'
+  package 'xfsprogs'
+  package 'xfslibs-dev'
+when 'redhat','centos','fedora','amazon'
+  package 'xfsprogs-devel'
+end
+
+directory "/srv/cassandra" do 
+	user 'root'
+	group 'root'
+	mode 00755
+	action :create
+end
+
+execute "create-file-system" do
+  user "root"
+  group "root"
+  command "mkfs.xfs /dev/xvdb"
+  not_if 'mount -l | grep /srv/cassandra'  # NB we grep for the mount-point *not* the device name b/c it'll show as /dev/xvdc or similar not the "role" name mount
+end
+
+mount "/srv/cassandra" do
+  device '/dev/xvdb'
+  fstype 'xfs'
+  options "noatime,nobootwait"
+  action [:mount, :enable]
+end
+
+execute "update_initramfs" do
+  user "root"
+  command "/usr/sbin/update-initramfs -u"
+  action :nothing
+end
