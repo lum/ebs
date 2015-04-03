e1f5aa564830e1ab0da5150bebc25ec81464b833
Steve <steve.lum@gmail.com>
2015-04-03 13:01:31 -0700

    updated to 0.4.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'steve@tempo.ai'
 license          'All rights reserved'
 description      'Installs/Configures ebs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.9'
+version          '0.4.0'
 
 depends "aws"
